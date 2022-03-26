e4faadb2f5072fb6ed4d3227223a43b83dcf6e3d
JinShengye <s-jin@seaos.co.jp>
2022-03-26 17:53:46 +0900

    ADD new robot class

--- jetbot/robot.py
+++ jetbot/robot.py
@@ -1,48 +1,26 @@
-import time
-import traitlets
-from traitlets.config.configurable import SingletonConfigurable
-from Adafruit_MotorHAT import Adafruit_MotorHAT
-from .motor import Motor
+from .motor_driver import MotorDriver
 
+class Robot():
+    def __init__(self):
+        self.__motor = MotorDriver()
 
-class Robot(SingletonConfigurable):
-    
-    left_motor = traitlets.Instance(Motor)
-    right_motor = traitlets.Instance(Motor)
+    def forward(self, speed):
+        self.__motor.motor_run(self.__motor.MOTOR_L.value,self.__motor.DIR_FORWARD.value,speed)
+        self.__motor.motor_run(self.__motor.MOTOR_R.value,self.__motor.DIR_FORWARD.value,speed)
 
-    # config
-    i2c_bus = traitlets.Integer(default_value=1).tag(config=True)
-    left_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
-    left_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
-    right_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
-    right_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
-    
-    def __init__(self, *args, **kwargs):
-        super(Robot, self).__init__(*args, **kwargs)
-        self.motor_driver = Adafruit_MotorHAT(i2c_bus=self.i2c_bus)
-        self.left_motor = Motor(self.motor_driver, channel=self.left_motor_channel, alpha=self.left_motor_alpha)
-        self.right_motor = Motor(self.motor_driver, channel=self.right_motor_channel, alpha=self.right_motor_alpha)
-        
-    def set_motors(self, left_speed, right_speed):
-        self.left_motor.value = left_speed
-        self.right_motor.value = right_speed
-        
-    def forward(self, speed=1.0, duration=None):
-        self.left_motor.value = speed
-        self.right_motor.value = speed
-
-    def backward(self, speed=1.0):
-        self.left_motor.value = -speed
-        self.right_motor.value = -speed
-
-    def left(self, speed=1.0):
-        self.left_motor.value = -speed
-        self.right_motor.value = speed
-
-    def right(self, speed=1.0):
-        self.left_motor.value = speed
-        self.right_motor.value = -speed
+    def backward(self, speed):
+        self.__motor.motor_run(self.__motor.MOTOR_L.value,self.__motor.DIR_BACKWARD.value,speed)
+        self.__motor.motor_run(self.__motor.MOTOR_R.value,self.__motor.DIR_BACKWARD.value,speed)
 
     def stop(self):
-        self.left_motor.value = 0
-        self.right_motor.value = 0
+        self.__motor.motor_stop(self.__motor.MOTOR_L.value)
+        self.__motor.motor_stop(self.__motor.MOTOR_R.value)
+
+    def left(self,speed):
+        self.__motor.motor_run(self.__motor.MOTOR_L.value,self.__motor.DIR_SLOWLEFT.value,speed)
+        self.__motor.motor_run(self.__motor.MOTOR_R.value,self.__motor.DIR_SLOWLEFT.value,speed/2)
+
+    def right(self,speed):
+        self.__motor.motor_run(self.__motor.MOTOR_L.value,self.__motor.DIR_SLOWRIGHT.value,speed)
+        self.__motor.motor_run(self.__motor.MOTOR_R.value,self.__motor.DIR_SLOWRIGHT.value,speed/2)
+
