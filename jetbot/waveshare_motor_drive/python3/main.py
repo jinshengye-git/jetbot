dae3713609b2efbeb6d47d3967cbcce4cce6b734
Jin Shengye <shengye.j@gmail.com>
2022-03-26 10:58:40 +0900

    UPD fix enum bugs

--- jetbot/waveshare_motor_drive/python3/main.py
+++ jetbot/waveshare_motor_drive/python3/main.py
@@ -6,15 +6,17 @@ from enum import Enum
 
 BRAKE_SMOOTHER = [0.]
 class DIR(Enum):
-    FORWARD :  'forward'    
-    BACKWARD:  'backward'
-    SPINLEFT:  'spinleft'
-    SPINRIGHT: 'spinright'
-    SLOWLEFT:  'slowleft'
-    SLOWRIGHT: 'slowright'
+    FORWARD =  'forward'    
+    BACKWARD=  'backward'
+    SPINLEFT=  'spinleft'
+    SPINRIGHT= 'spinright'
+    SLOWLEFT=  'slowleft'
+    SLOWRIGHT= 'slowright'
 class MOTOR(Enum):
-    LEFT:  0
-    RIGHT: 1
+    LEFT=  0
+    RIGHT= 1
+    MAX_VAL=100
+
 
 pwm = PCA9685(0x40, debug=False)
 pwm.setPWMFreq(50)
@@ -26,33 +28,41 @@ class MotorDriver():
         self.BIN1 = 3
         self.BIN2 = 4
         self.PWMB = 5
-
+        self.MOTOR_L=MOTOR.LEFT
+        self.MOTOR_R=MOTOR.RIGHT
+        self.MOTOR_MAX_VAL=MOTOR.MAX_VAL
+        self.DIR_FORWARD=DIR.FORWARD
+        self.DIR_BACKWARD=DIR.BACKWARD
+        self.DIR_SPINLEFT=DIR.SPINLEFT
+        self.DIR_SPINRIGHT=DIR.SPINRIGHT
+        self.DIR_SLOWLEFT=DIR.SLOWLEFT
+        self.DIR_SLOWRIGHT=DIR.SLOWRIGHT
     def MotorRun(self, motor, index, speed):
         #  DC Motor :
         #  
         if speed > 100:
             print('Speed should be 1 ~ 100')
             return
-        if motor == MOTOR.LEFT: #Left Motor
+        if motor == self.MOTOR_L.value: #Left Motor
             pwm.setDutycycle(self.PWMA, speed)
-            if index == DIR.FORWARD: # forward
+            if index == self.DIR_FORWARD.value: # forward
                 pwm.setLevel(self.AIN1, 0)
                 pwm.setLevel(self.AIN2, 1)
-            elif index == DIR.BACKWARD : # backward
+            elif index == self.DIR_BACKWARD.value : # backward
                 pwm.setLevel(self.AIN1, 1)
                 pwm.setLevel(self.AIN2, 0)
-            elif index == DIR.SPINLEFT: # spinleft
+            elif index == self.DIR_SPINLEFT.value: # spinleft
                 pwm.setLevel(self.AIN1, 0)
                 pwm.setLevel(self.AIN2, 1)
-            elif index == DIR.SPINRIGHT: # spinright
+            elif index == self.DIR_SPINRIGHT.value: # spinright
                 pwm.setLevel(self.AIN1, 1)
                 pwm.setLevel(self.AIN2, 0)
-            elif index == DIR.SLOWLEFT: # slowleft
+            elif index == self.DIR_SLOWLEFT.value: # slowleft
                 speed = speed * 0.8
                 pwm.setDutycycle(self.PWMA, speed)
                 pwm.setLevel(self.AIN1, 0)
                 pwm.setLevel(self.AIN2, 1)
-            elif index == DIR.SLOWRIGHT: # slowright
+            elif index == self.DIR_SLOWRIGHT.value: # slowright
                 speed = speed * 0.8
                 pwm.setDutycycle(self.PWMA, speed)
                 pwm.setLevel(self.AIN1, 1)
@@ -60,26 +70,26 @@ class MotorDriver():
             else:
                 print('Wrong dir command')
                 return                
-        elif motor == MOTOR.RIGHT: #Right Motor
+        elif motor == self.MOTOR_R.value: #Right Motor
             pwm.setDutycycle(self.PWMB, speed)
-            if index == DIR.FORWARD:
+            if index == self.DIR_FORWARD.value:
                 pwm.setLevel(self.BIN1, 0)
                 pwm.setLevel(self.BIN2, 1)
-            elif index == DIR.BACKWARD:
+            elif index == self.DIR_BACKWARD.value:
                 pwm.setLevel(self.BIN1, 1)
                 pwm.setLevel(self.BIN2, 0)
-            elif index == DIR.SPINLEFT:
+            elif index == self.DIR_SPINLEFT.value:
                 pwm.setLevel(self.BIN1, 0)
                 pwm.setLevel(self.BIN2, 1)
-            elif index == DIR.SPINRIGHT:
+            elif index == self.DIR_SPINRIGHT.value:
                 pwm.setLevel(self.BIN1, 1)
                 pwm.setLevel(self.BIN2, 0)
-            elif index == DIR.SLOWLEFT:
+            elif index == self.DIR_SLOWLEFT.value:
                 speed = speed * 0.8
                 pwm.setDutycycle(self.PWMB, speed)
                 pwm.setLevel(self.BIN1, 0)
                 pwm.setLevel(self.BIN2, 1)
-            elif index == DIR.SLOWRIGHT:
+            elif index == self.DIR_SLOWRIGHT.value:
                 speed = speed * 0.8
                 pwm.setDutycycle(self.PWMB, speed)
                 pwm.setLevel(self.BIN1, 1)
@@ -92,25 +102,25 @@ class MotorDriver():
             return
 
     def MotorStop(self, motor):
-        if motor == MOTOR.LEFT:
+        if motor == self.MOTOR_L.value:
             pwm.setDutycycle(self.PWMA, pulse=0) #pulse 0 ~ 100
-        elif motor == MOTOR.RIGHT:
+        elif motor == self.MOTOR_R.value:
             pwm.setDutycycle(self.PWMB, pulse=0)
 
     def MotorSmootherStop(self, motor):
-        if motor == MOTOR.LEFT:
+        if motor == self.MOTOR_L.value:
             pwm.setDutycycle(self.PWMA, pulse=70)
             pwm.setDutycycle(self.PWMA, pulse=40)
             pwm.setDutycycle(self.PWMA, pulse=10)
             pwm.setDutycycle(self.PWMA, pulse=0)
-        elif motor == MOTOR.RIGHT:
+        elif motor == self.MOTOR_R.value:
             pwm.setDutycycle(self.PWMB, pulse=70)
             pwm.setDutycycle(self.PWMB, pulse=40)
             pwm.setDutycycle(self.PWMB, pulse=10)
             pwm.setDutycycle(self.PWMB, pulse=0)
 
 
-"""
+
 try:
     Motor = MotorDriver()
     # control 2 motor
@@ -128,5 +138,5 @@ except KeyboardInterrupt:
     Motor.MotorRun(0, 'forward', 0)
     Motor.MotorRun(1, 'backward', 0)
     exit()
-"""
+
 
